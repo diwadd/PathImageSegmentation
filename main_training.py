3968f1a646b3f5204ea8a511d0830d510cef821d
Dawid Dul <dawid.dul@gmail.com>
2017-07-16 13:09:01 +0200

    Moved training code from main.py to main_training.

--- main_training.py
+++ main_training.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     nh = int(np.sqrt(m))
 
     random.shuffle(data_file_names)
-    train_fraction = 0.5
+    train_fraction = 0.6
     valid_fraction = 1.0 - train_fraction
     test_fraction = 0.5
 
@@ -98,6 +98,8 @@ if __name__ == "__main__":
     print("Number of train data files: " + str(len(x_train_fnl)))
     print("Number of valid data files: " + str(len(x_valid_fnl)))
     print("Number of test data files: " + str(len(x_test_fnl)))
+
+    K.get_session()
 
     model = dm.basic_model(iw=iw, 
                            ih=ih, 
@@ -111,18 +113,26 @@ if __name__ == "__main__":
     n = len(x_train_fnl)
     number_of_image_loads = round(n / noli)
     print("Number of image loads: " + str(number_of_image_loads))
-    n_epochs = 100    
-    n_sub_epochs = 1
+    n_epochs = 20    
+    n_sub_epochs = 5
+
+    print("Pre training evaluation:")
+    evaluate(model, x_valid_fnl, noli=64)
 
     for i in range(n_epochs):
         print("Global epoch: " + str(i) + "/" + str(n_epochs))
         model.fit_generator(data_generator(data_file_names, noli=noli),
                             steps_per_epoch=number_of_image_loads,
                             epochs=n_sub_epochs)
+        print("Validation data mean loss: ")
         evaluate(model, x_valid_fnl, noli=64)
 
 
-    model.save("model.h5")
+        model.save("model.h5")
+
+    print("Test data mean loss: ")
+    evaluate(model, x_test_fnl, noli=64)
 
 
+    K.clear_session()
 
